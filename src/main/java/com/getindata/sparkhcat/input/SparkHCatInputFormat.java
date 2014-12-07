247dfa1634114a99512da0eac05c68f72a46bf99
Hadoop Consultants <hadoop@truecaller.com>
2014-12-07 15:52:20 +0100

    Early, dirty init commit

---
+++ src/main/java/com/getindata/sparkhcat/input/SparkHCatInputFormat.java
@@ -0,0 +1,39 @@
+package com.getindata.sparkhcat.input;
+
+import java.io.IOException;
+
+import java.util.List;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.spark.SerializableWritable;
+import org.apache.hive.hcatalog.mapreduce.HCatInputFormat;
+import org.apache.hive.hcatalog.data.HCatRecord;
+
+
+public class SparkHCatInputFormat extends InputFormat<SerializableWritable<Writable>, HCatRecord> {
+	
+	private final HCatInputFormat input;
+	
+	public SparkHCatInputFormat() {
+		input = new HCatInputFormat();
+	}
+ 
+	@Override
+	public RecordReader<SerializableWritable<Writable>, HCatRecord> createRecordReader(
+			InputSplit arg0, TaskAttemptContext arg1) throws IOException,
+			InterruptedException {
+		return new SparkHCatRecordReader(input.createRecordReader(arg0, arg1));
+	}
+ 
+	@Override
+	public List<InputSplit> getSplits(JobContext arg0) throws IOException,
+			InterruptedException {
+		return input.getSplits(arg0);
+	}
+ 
+}
---
+++ src/main/java/com/getindata/sparkhcat/input/SparkHCatRecordReader.java
@@ -0,0 +1,54 @@
+package com.getindata.sparkhcat.input;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hive.hcatalog.data.HCatRecord;
+import org.apache.spark.SerializableWritable;
+
+class SparkHCatRecordReader extends RecordReader<SerializableWritable<Writable>, HCatRecord>  {
+	@SuppressWarnings("rawtypes")
+	private final RecordReader<WritableComparable, HCatRecord> reader;
+	
+	public SparkHCatRecordReader(RecordReader<WritableComparable, HCatRecord> reader) {
+		this.reader = reader;
+	}
+ 
+	@Override
+	public void close() throws IOException {
+		reader.close();
+	}
+ 
+	@Override
+	public SerializableWritable<org.apache.hadoop.io.Writable> getCurrentKey() throws IOException,
+			InterruptedException {
+		return new SerializableWritable<org.apache.hadoop.io.Writable>(reader.getCurrentKey());
+	}
+ 
+	@Override
+	public HCatRecord getCurrentValue() throws IOException,
+			InterruptedException {
+		return reader.getCurrentValue();
+	}
+ 
+	@Override
+	public float getProgress() throws IOException, InterruptedException {
+		return reader.getProgress();
+	}
+ 
+	@Override
+	public void initialize(InputSplit arg0, TaskAttemptContext arg1)
+			throws IOException, InterruptedException {
+		reader.initialize(arg0, arg1);
+	}
+ 
+	@Override
+	public boolean nextKeyValue() throws IOException, InterruptedException {
+		return reader.nextKeyValue();
+	}
+ 
+}
