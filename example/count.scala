883ddb8d56b862eaf6e022cc92e2da85f7d55dd5
kawaa <kawa.adam@gmail.com>
2015-01-22 08:52:08 +0000

    processing multiple partitions in example

--- example/count.scala
+++ example/count.scala
@@ -5,7 +5,7 @@ val v = classOf[org.apache.hive.hcatalog.data.HCatRecord]
 val conf = new org.apache.hadoop.conf.Configuration()
 conf.set("hive.metastore.uris", "thrift://hdp3:9083");
 
-org.apache.hive.hcatalog.mapreduce.HCatInputFormat.setInput(conf, "default", "spark_hcat_test", "type='5200'")
+org.apache.hive.hcatalog.mapreduce.HCatInputFormat.setInput(conf, "default", "spark_hcat_test", "type='46' or type='5200'")
 
 val table = sc.newAPIHadoopRDD(conf, f, k, v)
 
